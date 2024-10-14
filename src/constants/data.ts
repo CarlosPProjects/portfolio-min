b862835bb3da831977b87f2baee81ba816727c17
CarlosPProjects <cg9822@gmail.com>
2024-10-14 21:28:13 +0200

    cmm

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -1,3 +1,9 @@
-export const social = [{
-  
+export const social:ISocial[] = [{
+  name: 'GitHub',
+  icon: 'github',
+  url: 'https://github.com/withastro/astro'
+},{
+  name: 'Twitter',
+  icon: 'twitter',
+  url: 'https://twitter.com/withastro'
 }]
