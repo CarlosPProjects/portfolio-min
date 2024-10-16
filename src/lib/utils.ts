a4c9ad314c96d400b9517a1e3ddb398cdeec9f89
CarlosPProjects <cg9822@gmail.com>
2024-10-16 09:19:17 +0200

    "Added mobile menu toggle functionality to Header component"

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const toggleMenu = () => {
+  const menuContainer = document.getElementById("__menuContainer");
+
+  menuContainer?.classList.toggle("active");
+  console.log('Ready');
+};
